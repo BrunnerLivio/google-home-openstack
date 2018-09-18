9f017ae7260e66142823c4af0589095d36946aa2
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-09-18 10:34:27 +0200

    Add key pair when creating vm

--- src/plugins/openstack/create-vm.plugin.ts
+++ src/plugins/openstack/create-vm.plugin.ts
@@ -11,7 +11,6 @@ import { DialogflowResponse } from '../../common/dialogflow-response';
 import { OpenstackHumanService } from './openstack-human.service';
 import { FloatingIPCreateDto } from './interfaces';
 
-
 export type VMSize = 'small' | 'medium' | 'large';
 
 export interface CreateVMParameters {
@@ -99,8 +98,9 @@ export class CreateVMPlugin implements IGoogleHomePlugin {
             flavorRef,
             imageRef: imageRef,
             networks: [{
-                uuid: '8105a814-769e-46a2-96b1-7579195ad76f'
-            }]
+                uuid: this.config.defaultNetworkUUID
+            }],
+            key_name: this.config.defaultKeyPairName
         };
     }
 
@@ -114,7 +114,9 @@ export class CreateVMPlugin implements IGoogleHomePlugin {
             for (serverCreated = 0; serverCreated < serverCount; serverCreated++) {
                 const newServer = await this.openstack.createServer(server);
                 const floatingIp: FloatingIPCreateDto = await this.openstack.createFloatingIP(this.config.defaultFloatingIpPool);
-                await this.openstack.associateFloatingIp(newServer.id, floatingIp.ip);
+                // TODO: Check state of vm instead of timeout
+                console.log(newServer, floatingIp);
+                window.setTimeout(async () => await this.openstack.associateFloatingIp(newServer.id, floatingIp.ip), 1500);
             }
         }
         catch (err) {
